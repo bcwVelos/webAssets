d8860b4fa2385d20784c5e4b8dfec26464b2b6c4
apape2346 <adam.pape@bcw.edu>
2017-04-24 16:37:33 -0500

    removed function call

--- stage/js/deployed/account/1036_test_background_image.js
+++ stage/js/deployed/account/1036_test_background_image.js
@@ -3,14 +3,11 @@ $j(document).ready(function() {
 	var test_field = 'fld10050_121655_126118';
 
 	var test_box = document.getElementsByName(test_field);
-	
-	$j(function() {
-		$j("fld10050_121655_126118").position({
-			my: "center",
-			at: "center",
-			of: "#targetImage"
-		});
 		
+	$j("fld10050_121655_126118").position({
+		my: "center",
+		at: "center",
+		of: "#targetImage"
 	});
 	
 });
