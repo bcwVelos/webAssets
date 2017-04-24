95a90118f31fe5609c09f4611183ca6f91d2a588
apape2346 <adam.pape@bcw.edu>
2017-04-24 16:30:20 -0500

    more testing

--- stage/js/deployed/account/1036_test_background_image.js
+++ stage/js/deployed/account/1036_test_background_image.js
@@ -1,10 +1,14 @@
 $j(document).ready(function() {
+	<div id="targetImage">
+		<img src="../bcw/image/Dental Numbering System.jpg" alt="Tooth Pic" style="width:1501px;height:885px;"/>
+	</div>
+	
 	var test_field = 'fld10050_121655_126118';
 
 	var test_box = document.getElementsByName(test_field);
 	
-	$(function() {
-		$("fld10050_121655_126118").position({
+	$j(function() {
+		$j("fld10050_121655_126118").position({
 			my: "center",
 			at: "center",
 			of: "#targetImage"
