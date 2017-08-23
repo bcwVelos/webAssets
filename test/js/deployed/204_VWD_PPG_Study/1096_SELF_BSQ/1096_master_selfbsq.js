c79eab1bc4349b05f0dace5330e1b7f17ba0c7ec
apape2346 <adam.pape@bcw.edu>
2017-08-23 10:58:18 -0500

    updated field IDs

--- test/js/deployed/204_VWD_PPG_Study/1096_SELF_BSQ/1096_master_selfbsq.js
+++ test/js/deployed/204_VWD_PPG_Study/1096_SELF_BSQ/1096_master_selfbsq.js
@@ -2,78 +2,78 @@
  * 
  */
 noseBleedVariables = {
-	epistaxisTotalScore: 'fld10050_124703_129438',
-	epistaxisPrimary: 'fld10050_124704_129439'
+	epistaxisTotalScore: 'fld10050_125953_130720',
+	epistaxisPrimary: 'fld10050_125954_130721'
 }
 
 cutaneousVariables = {
-	bruisingTotalScore: 'fld10050_124706_129441',
-	bruisingPrimary: 'fld10050_124707_129442'
+	bruisingTotalScore: 'fld10050_125956_130723',
+	bruisingPrimary: 'fld10050_125957_130724'
 }
 
 minorWoundsVariables = {
-	minorWoundsTotalScore: 'fld10050_124709_129444',
-	minorWoundsPrimary: 'fld10050_124710_129445'
+	minorWoundsTotalScore: 'fld10050_125959_130726',
+	minorWoundsPrimary: 'fld10050_125960_130727'
 }
 
 hematuriaVariables = {
-	hematuriaTotalScore: 'fld10050_124712_129447',
-	hematuriaPrimary: 'fld10050_124713_129448'
+	hematuriaTotalScore: 'fld10050_125962_130729',
+	hematuriaPrimary: 'fld10050_125963_130730'
 }
 
 giBleedingVariables = {
-	giBleedingTotalScore: 'fld10050_124714_129449',
-	giBleedingPrimary: 'fld10050_124716_129451'
+	giBleedingTotalScore: 'fld10050_125964_130731',
+	giBleedingPrimary: 'fld10050_125966_130733'
 }
 
 oralCavityVariables = {
-	oralCavityTotalScore: 'fld10050_124717_129452',
-	oralCavityPrimary: 'fld10050_124719_129454'
+	oralCavityTotalScore: 'fld10050_125967_130734',
+	oralCavityPrimary: 'fld10050_125969_130736'
 }
 
 toothExtractionVariables = {
-	toothExtractionTotalScore: 'fld10050_124720_129455',
-	toothExtractionPrimary: 'fld10050_124722_129457'
+	toothExtractionTotalScore: 'fld10050_125970_130737',
+	toothExtractionPrimary: 'fld10050_125972_130739'
 }
 
 surgeryVariables = {
-	surgeryTotalScore: 'fld10050_124723_129458',
-	surgeryPrimary: 'fld10050_124725_129460'
+	surgeryTotalScore: 'fld10050_125973_130740',
+	surgeryPrimary: 'fld10050_125975_130742'
 }
 
 menstrualVariables = {
-	menstrualTotalScore: 'fld10050_124728_129463',
-	menstrualPrimary: 'fld10050_124729_129464'
+	menstrualTotalScore: 'fld10050_125978_130745',
+	menstrualPrimary: 'fld10050_125979_130746'
 }
 
 deliveryVariables = {
-	deliveryTotalScore: 'fld10050_124730_129465',
-	deliveryPrimary: 'fld10050_124732_129467'
+	deliveryTotalScore: 'fld10050_125980_130747',
+	deliveryPrimary: 'fld10050_125982_130749'
 }
 
 hematomaVariables = {
-	hematomaTotalScore: 'fld10050_124733_129468',
-	hematomaPrimary: 'fld10050_124735_129470'
+	hematomaTotalScore: 'fld10050_125983_130750',
+	hematomaPrimary: 'fld10050_125985_130752'
 }
 
 hemarthrosisVariables = {
-	hemarthrosisTotalScore: 'fld10050_124736_129471',
-	hemarthrosisPrimary: 'fld10050_124738_129473'
+	hemarthrosisTotalScore: 'fld10050_125986_130753',
+	hemarthrosisPrimary: 'fld10050_125988_130755'
 }
 
 cnsVariables = {
-	cnsTotalScore: 'fld10050_124740_129475',
-	cnsPrimary: 'fld10050_124741_129476'
+	cnsTotalScore: 'fld10050_125990_130757',
+	cnsPrimary: 'fld10050_125991_130758'
 }
 
 otherBleedingVariables = {
-	otherBleedingTotalScore: 'fld10050_124742_129477',
-	otherBleedingPrimary: 'fld10050_124744_129479'
+	otherBleedingTotalScore: 'fld10050_125992_130759',
+	otherBleedingPrimary: 'fld10050_125994_130761'
 }
 
 totalScoreVariables = {
-	totalScoreTop: 'fld10050_124673_129408',
-	totalScoreBottom: 'fld10050_124749_129484'
+	totalScoreTop: 'fld10050_125923_130690',
+	totalScoreBottom: 'fld10050_125999_130766'
 }
 
 var formStatus = document.getElementsByName('er_def_formstat');
