3f7d44175120cd8034018e62870695d329fd9ee6
apape2346 <adam.pape@bcw.edu>
2018-03-19 14:34:14 -0500

    testing pcs form

--- test/js/deployed/formLibrary/1130_PCS/1130_master_form_lib.js
+++ test/js/deployed/formLibrary/1130_PCS/1130_master_form_lib.js
@@ -1,8 +1,8 @@
 /**
  * 
  */
-pcsWorryField = 'fld10050_128106_133034';
-pcsCantGoOn = 'fld10050_128107_133035';
+var pcsWorryField = 'fld10050_128106_133034';
+/* pcsCantGoOn = 'fld10050_128107_133035';
 pcsTerrible = 'fld10050_128108_133036';
 pcsAwful = 'fld10050_128093_133024';
 pcsCantStand = 'fld10050_128109_133037';
@@ -14,7 +14,7 @@ pcsHowMuchHurts = 'fld10050_128114_133042';
 pcsPainToStop = 'fld10050_128115_133043';
 pcsIntensity = 'fld10050_128116_133044';
 pcsSerious = 'fld10050_128117_133045';
-pcsTotalScore = 'fld10050_128118_133046';
+pcsTotalScore = 'fld10050_128118_133046'; */
 
 var formStatus = document.getElementsByName('er_def_formstat');
 
