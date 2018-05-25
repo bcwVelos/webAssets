4a887fca8f9df0fe91a47cfad3cb5ce94e98b457
apape2346 <adam.pape@bcw.edu>
2018-05-25 13:04:06 -0500

    testing

--- test/js/deployed/944_SCD_Bank/1156_Medical_History/1156_medical_history.js
+++ test/js/deployed/944_SCD_Bank/1156_Medical_History/1156_medical_history.js
@@ -38,8 +38,8 @@ $j(document).ready(function() {
 	
 		//hide the comments label- never want to see it
 		$j('td:contains("Comments")').hide();
-		var jointReplacementYes = document.getElementsByName(jointReplacement)[0];
-		var jointReplacementNo = document.getElementsByName(jointReplacement)[1];
+		//var jointReplacementYes = document.getElementsByName(jointReplacement)[0];
+		//var jointReplacementNo = document.getElementsByName(jointReplacement)[1];
 		
 		//if (!jointReplacementYes.checked) {
 			$j("#" + surgeryRows[0].addRowBtn).hide();
