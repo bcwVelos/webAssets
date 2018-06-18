6ddc8628fe08fe1396a11c7dfda845727812cf02
apape2346 <adam.pape@bcw.edu>
2018-06-18 16:09:38 -0500

    testing calculating BMI

---
+++ test/js/deployed/formLibrary/1146_Vital_Signs/1146_vital_signs_calcBMI.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+calcBMIVariables = {
+	height: 'fld10050_131124_136056',
+	weight: 'fld10050_131125_136057',
+	BMI: 'fld10050_131126_136058',
+}
+
+var formStatus = document.getElementsByName('er_def_formstat');
+
+$j(document).ready(function() {
+	
+	$j('.[class^="calculateBtn"]').click(function(event) {
+		calcBMI();
+	});
+	
+	$j('.[id^="submit_btn"]').click(function(event) {
+		calcBMI();
+	});
+});
+
+function calcBMI() {
+
+	var BMIValue = 0;
+
+	var BMIHeight = document
+			.getElementsByName(calcBMIVariables.height);
+	var BMIWeight = document
+			.getElementsByName(calcBMIVariables.weight);
+	var BMIField = document
+			.getElementsByName(calcBMIVariables.BMI);
+	var calcButton = document
+			.getElementsByName(calcBMIVariables.calcButton);
+			
+	var heightValue = Number(BMIHeight[0].value);
+	var weightValue = Number(BMIWeight[0].value);
+}
