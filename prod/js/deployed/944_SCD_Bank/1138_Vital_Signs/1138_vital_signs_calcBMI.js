b98a62b41021b81da69f438d1ceb86b505f93a64
apape2346 <adam.pape@bcw.edu>
2018-06-22 14:33:49 -0500

    added vital signs form to SCD Bank study

---
+++ prod/js/deployed/944_SCD_Bank/1138_Vital_Signs/1138_vital_signs_calcBMI.js
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+
+calcBMIVariables = {
+	height: 'fld10050_128411_134423',
+	heightCM: 'fld10050_128422_134434',
+	weight: 'fld10050_128413_134425',
+	BMI: 'fld10050_128415_134427',
+}
+
+var formStatus = document.getElementsByName('er_def_formstat');
+
+$j(document).ready(function() {
+	
+	$j('.[class^="convertCMBtn"]').click(function(event) {
+		convertCM();
+	});
+	
+	$j('.[class^="calculateBtn"]').click(function(event) {
+		calcBMI();
+	});
+});
+
+function calcBMI() {
+
+	var BMIValue = 0;
+
+	var BMIHeight = document
+			.getElementsByName(calcBMIVariables.height);
+	var BMIWeight = document
+			.getElementsByName(calcBMIVariables.weight);
+	var BMIField = document
+			.getElementsByName(calcBMIVariables.BMI);
+			
+	var heightValue = Number(BMIHeight[0].value);
+	var weightValue = Number(BMIWeight[0].value);
+	
+	if (isNaN(heightValue) || isNaN(weightValue)) {
+			alert ("Please check the values entered in the Height (m) and Weight (kg) fields to confirm they are correct.");
+	} else {
+		BMIValue = weightValue/(heightValue * heightValue);
+	}
+	
+	BMIField[0].value = BMIValue.toFixed(1);
+	
+}
+
+function convertCM() {
+
+	var BMIValue = 0;
+
+	var BMIHeight = document
+			.getElementsByName(calcBMIVariables.height);
+	var BMIHeightCM = document
+			.getElementsByName(calcBMIVariables.heightCM);
+			
+	var heightValue = Number(BMIHeightCM[0].value);
+	
+	heightInM = heightValue/100;
+	
+	BMIHeight[0].value = heightInM;
+	
+}
