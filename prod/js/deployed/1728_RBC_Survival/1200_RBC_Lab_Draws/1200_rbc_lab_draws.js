20a48d6c3a563e2a788c9d68ea8c148169f8ad4d
Adam Pape <adam.pape@bcw.edu>
2020-01-30 16:51:16 -0600

    Update 1200_rbc_lab_draws.js

--- prod/js/deployed/1728_RBC_Survival/1200_RBC_Lab_Draws/1200_rbc_lab_draws.js
+++ prod/js/deployed/1728_RBC_Survival/1200_RBC_Lab_Draws/1200_rbc_lab_draws.js
@@ -1,14 +1,14 @@
 var dateRows = new Array(1);
 
 dateRows[0] = {
-	preDate : 'fld10050_138457_144669_span',  
-	5minDate : 'fld10050_138442_144654_span',
-	7minDate: 'fld10050_138464_144676_span',
-	10minDate : 'fld10050_138468_144680_span',
-	12minDate : 'fld10050_138477_144689_span',
-	15minDate : 'fld10050_138467_144679_span',
-	30minDate : 'fld10050_138483_144695_span',
-	1hrDate : 'fld10050_138489_144701_span',
+	ifPreDate : 'fld10050_138457_144669_span',  
+	if5minDate : 'fld10050_138442_144654_span',
+	if7minDate: 'fld10050_138464_144676_span',
+	if10minDate : 'fld10050_138468_144680_span',
+	if12minDate : 'fld10050_138477_144689_span',
+	if15minDate : 'fld10050_138467_144679_span',
+	if30minDate : 'fld10050_138483_144695_span',
+	if1hrDate : 'fld10050_138489_144701_span',
 	tfPreDate : 'fld10050_138508_144720_span',
 	tf1hrDate : 'fld10050_138510_144722_span',
 	tf4hrDate : 'fld10050_138514_144726_span',
