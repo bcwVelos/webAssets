87a3984acffb511f30a73ae35522887242d6d9b7
Adam Pape <adam.pape@bcw.edu>
2020-01-30 16:28:32 -0600

    Create 1200_rbc_lab_draws.js

---
+++ prod/js/deployed/1728_RBC_Survival/1200_RBC_Lab_Draws/1200_rbc_lab_draws.js
@@ -0,0 +1,30 @@
+var preDate = 'fld10050_138457_144669_span';
+var 5minDate = 'fld10050_138442_144654_span';
+var 7minDate = 'fld10050_138464_144676_span';
+var 10minDate = 'fld10050_138468_144680_span';
+var 12minDate = 'fld10050_138477_144689_span';
+var 15minDate = 'fld10050_138467_144679_span';
+var 30minDate = 'fld10050_138483_144695_span';
+var 1hrDate = 'fld10050_138489_144701_span';
+
+var tfpreDate = 'fld10050_138508_144720_span';
+var tf1hrDate = 'fld10050_138510_144722_span';
+var tf4hrDate = 'fld10050_138514_144726_span';
+
+var copyBtn = 'fld10050_138539_144752_id';
+
+$j(document).ready(function() {
+	
+$j(".copyBtn").click(function() {
+	
+	var parentLabel = $j(this).parent("label");
+	var labelId = $j(parentLabel).attr("id");
+
+	var x;
+	var tempDate = $j("#" + preDate).val();
+
+	alert("tempDate");
+});
+
+
+});
