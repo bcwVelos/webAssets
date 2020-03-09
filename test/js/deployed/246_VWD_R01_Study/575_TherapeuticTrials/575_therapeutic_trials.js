16d1ee8020e6e64b6a3a72ed9a1939df720c1770
Adam Pape <adam.pape@bcw.edu>
2020-03-09 13:56:07 -0500

    Update 575_therapeutic_trials.js
    testing

--- test/js/deployed/246_VWD_R01_Study/575_TherapeuticTrials/575_therapeutic_trials.js
+++ test/js/deployed/246_VWD_R01_Study/575_TherapeuticTrials/575_therapeutic_trials.js
@@ -1,5 +1,5 @@
 //********* BEGIN DATA OBJECT FUNCTIONS **********//
-alert("Hello 2");
+alert("Hello 2");/*
 //Define Object that will hold the the ids to represent each section of a repeating row
 //Create 2 constructor methods - one is empty, the other takes in all the values as parameters.
 function medicationRow(){
@@ -269,4 +269,4 @@ $j(".deleteBtn").click(function() {
 });
 
 });
-
+*/
