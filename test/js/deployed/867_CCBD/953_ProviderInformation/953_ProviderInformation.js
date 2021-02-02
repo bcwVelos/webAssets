41431acfacad2fc8858da09520304ed8c6030273
Adam Pape <APape@versiti.org>
2021-02-02 14:50:11 -0600

    Create 953_ProviderInformation.js

---
+++ test/js/deployed/867_CCBD/953_ProviderInformation/953_ProviderInformation.js
@@ -0,0 +1,26 @@
+<script language="JavaScript" type="text/javascript">
+function Init() {
+  var aa= document.getElementsByTagName("input");
+  for (var i =0; i < aa.length; i++){
+  if (aa[i].type == 'checkbox' && aa[i].id == "<b>Provider Not in System</b>") {
+  if (aa[i].addEventListener) {
+aa[i].addEventListener("CheckboxStateChange",  document.getElementsByTagName("input");
+ for (var i =0; i < aa.length; i++){
+        if (aa[i].type == 'checkbox' && aa[i].id == "<b>Provider Not in System</b>") {
+aa[i].attachEvent(" "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Provider to Velos&body=test" + "&body=Provider Name:'/r'Patient ID:";
+    window.location.href = link;
+} }
+function  event.srcElement;
+  if (checkbox.checked) {
+    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Provider to Velos&body=test" + "&body=Provider Name:'/r'Patient ID:";
+    window.location.href = link;
+} }
+window. function()
+{
+  if (document.addEventListener) {
+    Init();
+  } else {
+    InitIE8();
+  }
+}
+</script>
