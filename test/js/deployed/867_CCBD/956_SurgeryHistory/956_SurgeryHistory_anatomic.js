39b1668402bf7e2ffac9a5678d0f8fc2a5ee24bf
Adam Pape <APape@versiti.org>
2023-07-19 12:43:19 -0500

    Update 956_SurgeryHistory_anatomic.js

--- test/js/deployed/867_CCBD/956_SurgeryHistory/956_SurgeryHistory_anatomic.js
+++ test/js/deployed/867_CCBD/956_SurgeryHistory/956_SurgeryHistory_anatomic.js
@@ -1,14 +1,14 @@
 function Init() {
   var aa= document.getElementsByTagName("input");
   for (var i =0; i < aa.length; i++){
-  if (aa[i].type == 'checkbox' && aa[i].id == "<b>Provider Not in System</b>") {
+  if (aa[i].type == 'checkbox' && aa[i].id == "Anatomic Site Not in List") {
   if (aa[i].addEventListener) {
 aa[i].addEventListener("CheckboxStateChange", OnChangeCheckbox,false);
 } } } }
 function InitIE8() {
  var aa= document.getElementsByTagName("input");
  for (var i =0; i < aa.length; i++){
-     if (aa[i].type == 'checkbox' && aa[i].id == "<b>Provider Not in System</b>") {
+     if (aa[i].type == 'checkbox' && aa[i].id == "Anatomic Site Not in List") {
   aa[i].attachEvent("onclick",OnChangeCheckboxIE8);
        }
     }
@@ -16,13 +16,13 @@ function InitIE8() {
 function OnChangeCheckbox(event) {
   var checkbox = event.target;
   if (checkbox.checked) {
-    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Provider to Velos&body=test" + "&body=Provider Name:%0D%0APatient ID:";
+    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Anatomic Site to Velos&body=test" + "&body=Anatomic Site:%0D%0APatient ID:";
     window.location.href = link;
 } }
 function OnChangeCheckboxIE8(event) {
   var checkbox = event.srcElement;
   if (checkbox.checked) {
-    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Provider to Velos&body=test" + "&body=Provider Name:%0D%0APatient ID:";
+    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Anatomic Site to Velos&body=test" + "&body=Anatomic Site:%0D%0APatient ID:";
     window.location.href = link;
 } }
 window.onload = function()
