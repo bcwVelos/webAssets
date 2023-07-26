d191facc5c3777bb87bc2baa299107c9069371f7
Adam Pape <APape@versiti.org>
2023-07-26 14:50:53 -0500

    added one inclusive file instead of two separate

--- test/js/deployed/867_CCBD/956_SurgeryHistory/956_SurgeryHistory_surgeryanatomicsite.js
+++ test/js/deployed/867_CCBD/956_SurgeryHistory/956_SurgeryHistory_surgeryanatomicsite.js
@@ -1,14 +1,14 @@
 function Init() {
   var aa= document.getElementsByTagName("input");
   for (var i =0; i < aa.length; i++){
-  if (aa[i].type == 'checkbox' && aa[i].id == "Anatomic Site Not in List") {
+  if (aa[i].type == 'checkbox' && aa[i].id == "Surgery or Anatomic Site Not in List") {
   if (aa[i].addEventListener) {
 aa[i].addEventListener("CheckboxStateChange", OnChangeCheckbox,false);
 } } } }
 function InitIE8() {
  var aa= document.getElementsByTagName("input");
  for (var i =0; i < aa.length; i++){
-     if (aa[i].type == 'checkbox' && aa[i].id == "Anatomic Site Not in List") {
+     if (aa[i].type == 'checkbox' && aa[i].id == "Surgery or Anatomic Site Not in List") {
   aa[i].attachEvent("onclick",OnChangeCheckboxIE8);
        }
     }
@@ -16,13 +16,13 @@ function InitIE8() {
 function OnChangeCheckbox(event) {
   var checkbox = event.target;
   if (checkbox.checked) {
-    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Anatomic Site to Velos&body=test" + "&body=Anatomic Site:%0D%0APatient ID:";
+    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Surgery to Velos&body=test" + "&body=Surgery Name:%0D%0AAnatomic Site:%0D%0APatient ID:";
     window.location.href = link;
 } }
 function OnChangeCheckboxIE8(event) {
   var checkbox = event.srcElement;
   if (checkbox.checked) {
-    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Anatomic Site to Velos&body=test" + "&body=Anatomic Site:%0D%0APatient ID:";
+    var link = "mailto:velossupport@bcw.edu?subject=Encrypt Request to Add Surgery to Velos&body=test" + "&body=Surgery Name:%0D%0AAnatomic Site:%0D%0APatient ID:";
     window.location.href = link;
 } }
 window.onload = function()
